5fa0c0bd40db05ea279f642ba3c8ea52d17ee98b
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2021-03-26 12:54:18 -0400

    outline permutation code draft

--- notebooks/stitches_dev/permuting_matches.R
+++ notebooks/stitches_dev/permuting_matches.R
@@ -51,3 +51,78 @@ get_num_perms <- function(matched_data){
   return(list(data.frame(totalNumPerms = prod(num_matches$n_matches)),
               num_matches))
 }
+
+
+###################################################################
+
+# A function to sample from input `matched_data` (the the results 
+# of `match_neighborhood(target, archive)` to produce permutations
+# of possible stitching recipes that will match the target data.
+#
+# Args:
+# matched_data: data output from match_neighborhood.
+# N_matches: the number of desired stitching recipes.
+# optional something: a previous output of this function that contains a 
+# list of already created recipes to avoid re-making.
+# 
+# Returns: something
+
+permute_stitching_recipes <- function(N_matches, matched_data, optional=NULL){
+
+  N_to_make <- N_matches
+  
+  N_data_max <- get_num_perms(matched_data)[[1]]$totalNumPerms
+  if (N_matches > N_data_max){
+    message(paste("You have requested more recipes than possible, returning", N_data_max))
+    N_to_make <- N_data_max
+  }
+  
+  
+  
+  
+  # Initialize the number of matches to either 0 or the input read from optional:
+  if( is.character(optional)){
+    # initialize to the read-in:
+    recipes <- read.csv(optional)
+
+    # message: you requested N_matches
+    # and Have NN new ones, will produce NN-N_matches new ones and then just 
+    # Update N_matches to be NN-N_matches.
+    # TODO need to relate the N_to_make here to N_data_max
+    N_existing <- length(unique(recipes$stitching_id))
+    
+    message(paste('You requested', N_matches, 'and already have', N_existing))
+    if (N_matches-N_existing <= 0){
+      message("You have all you need")
+      return(recipes)
+    } else {
+      message(paste("Creating", N_matches-N_existing, "new recipes and adding to those input."))
+      N_to_make <- N_matches-N_existing
+    }
+  } else{
+    # initialize to empty
+    recipes <- data.frame()
+  }
+  
+  
+  # Somehow sample and construct, compare to existing recipes, keep if new, give an id, add to recipes
+  # and repeat....
+   
+  ## TODO doing it this way, may not even need N_to_make.
+  while(length(unique(recipes$stitching_id)) < N_matches){
+    # Sample
+    
+    # compare
+    
+    # label
+    
+    # concat
+    
+  }
+  
+  
+  
+}
+
+
+

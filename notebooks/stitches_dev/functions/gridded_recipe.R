957f3597c57847373f6e28beb4ee2218c6aa0776
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2021-05-06 13:49:51 -0400

    fix typos

--- notebooks/stitches_dev/functions/gridded_recipe.R
+++ notebooks/stitches_dev/functions/gridded_recipe.R
@@ -1,12 +1,12 @@
 
-# Go through the recepie and when there is a transition period, aka the archive years span both the 
-# historical and future scnearios go through and instert in an extra period so that they don't do 
+# Go through the recipe and when there is a transition period, aka the archive years span both the 
+# historical and future scenarios go through and insert in an extra period so that they don't do 
 # this over lap any more. 
 # Args 
-#   rp: data frame of the recepie 
-# Return: a data frame of of the recpie with no over lapping historical/future experiments, 
+#   rp: data frame of the recipe
+# Return: a data frame of of the recipe with no over lapping historical/future experiments, 
 # this is now ready to join with pangeo information. 
-handel_transition_periods <- function(rp){
+handle_transition_periods <- function(rp){
   
   apply(rp, 1, function(x){
     
@@ -14,8 +14,8 @@ handel_transition_periods <- function(rp){
     transition_period <- 2014 %in% x["archive_start_yr"][[1]]:x["archive_end_yr"][[1]]
     
     # If the period is a transition period aka it contains both historical and future years. 
-    # Then we will need to break it out into two periods to refelct the different files 
-    # we will neecd to pull from pangeo. 
+    # Then we will need to break it out into two periods to reflect the different files 
+    # we will need to pull from pangeo. 
     if(transition_period){
       target_yrs <- x["target_start_yr"][[1]]:x["target_end_yr"][[1]]
       
@@ -83,14 +83,14 @@ handel_transition_periods <- function(rp){
 
 
 
-# Go through a recepie and ensure that all of the periods have the same archive 
+# Go through a recipe and ensure that all of the periods have the same archive 
 # and target period length, if not update to reflect the target period length. 
 # Otherwise you'll end up with extra years in the stitched data. This is really 
 # only an issue for the final period of target data because sometimes that period is somewhat short. 
 # Args 
-# rp: a data frame of the recepie 
-# Return: a recepie data frame that has target and archive periods of the same length.
-handel_final_period <- function(rp){
+# rp: a data frame of the recipe
+# Return: a recipe data frame that has target and archive periods of the same length.
+handle_final_period <- function(rp){
   
   out <- data.frame(stringsAsFactors = FALSE)
   
@@ -117,7 +117,7 @@ handel_final_period <- function(rp){
     } else {
       # TODO need to revisit, just added an extra year to the archive length but that seems 
       # pretty sus. 
-      # Figure out how much shorter the target period is than the archive period. 
+      # Figure out how much longer the target period is than the archive period. 
       diff <- len_archive - len_target
       updated <- rp[row, ]
       updated$archive_start_yr <- updated$archive_start_yr - 1
@@ -134,7 +134,7 @@ handel_final_period <- function(rp){
 
 
 
-# Generate recpcie for the stiched gridded product 
+# Generate recipe for the stiched gridded product 
 # (based on the notebook 5 code)
 # Args 
 #   target_data: a data frame of the target data 
@@ -144,7 +144,7 @@ handel_final_period <- function(rp){
 # Return: data frame containing the following columns, stitching_id, target_start_yr, target_end_yr, 
 #     archive_start_yr, archive_end_yr, and file. This is all of the information that will be needed 
 #     by python to stitch the gridded product. 
-generate_gridded_recepie <- function(target_data, archive_data, n, tol = 0.1){
+generate_gridded_recipe <- function(target_data, archive_data, n, tol = 0.1){
   
   # Load the complete archive. 
   complete_archive <- read.csv("inputs/main_raw_pasted_tgav_anomaly_all_pangeo_list_models.csv", 
@@ -163,29 +163,29 @@ generate_gridded_recepie <- function(target_data, archive_data, n, tol = 0.1){
                                      tol=tol)
   
   # Convert them to a sample of individual Tgav Recipes:
-  permute_stitching_recipes(N_matches = 5, matched_data = matched_data, archive = archive_data) %>% 
+  permute_stitching_recipes(N_matches = n, matched_data = matched_data, archive = archive_data) %>% 
     dplyr::select(target_start_yr, target_end_yr, archive_experiment,archive_variable,
                   archive_model, archive_ensemble, archive_start_yr, archive_end_yr, archive_year, stitching_id) -> 
-    unformatted_recepie 
+    unformatted_recipe 
   
   
   # Because of the way that we've removed the historical achive but will want to pull from pangeo 
   # we need to properly treat the historical years.
-  unformatted_recepie %>% 
+  unformatted_recipe %>% 
     # When a archive period spans the historical and future scenario we need to make sure there is 
     # it is split up into seprate periods otherwise it will be impossible to pair the chunks with 
     # the pangeo data.
-    handel_transition_periods %>% 
+    handle_transition_periods %>% 
     # Make sure that the length of the target and archive periods are the same to prevent 
     # extra data from being assigned to that period. 
-    handel_final_period %>% 
+    handle_final_period %>% 
     # Make sure that if there are historical years of data being used assign the experiment name 
     # to historical. 
     mutate(archive_experiment = ifelse(archive_end_yr <= 2014, 'historical', archive_experiment)) -> 
-    formatted_recepie 
+    formatted_recipe 
   
   # Now add the pangeo file information! 
-  formatted_recepie %>%  
+  formatted_recipe %>%  
     left_join(complete_archive, by = c("archive_experiment" = "experiment", 
                                        "archive_model" = "model", 
                                        "archive_ensemble" = "ensemble")) %>% 

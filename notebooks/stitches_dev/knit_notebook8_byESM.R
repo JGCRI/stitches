c003ce916ec9c9570b834d9bc55b6988446b11ff
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2021-06-03 11:09:41 -0400

    miroc ssp370 and mpi-hr ssp245. 10draw results

--- notebooks/stitches_dev/knit_notebook8_byESM.R
+++ notebooks/stitches_dev/knit_notebook8_byESM.R
@@ -3,27 +3,31 @@
 # what we will emulate:
 esm_name_vec <- c("ACCESS-ESM1-5",
               "CanESM5",
-              "GISS-E2-1-G",
+              #"GISS-E2-1-G", # no psl or pr data on pangeo
               "MIROC6",
               "MPI-ESM1-2-HR",
               "MPI-ESM1-2-LR",
               "NorCPM1",
-              "UKESM1-0-LL"  )
+              "UKESM1-0-LL")
 esm_experiment_vec <- c('ssp245', 'ssp370')
 
 # How many times do we want to draw full generated ense mbles:
 Ndraws <- 10
 
-for (i in 2:2){# 1:length(esm_name_vec)){
-  for (j in 1:1){#1:length(esm_experiment_vec)){
+for (name_ind in 2:length(esm_name_vec)){
+  for (exp_ind in 1:length(esm_experiment_vec)){
     
     rm(esm_name, esm_experiment)
     
     # define our target esm name and experiment values
     # as global variables so that the markdown can access
-    esm_name <- esm_name_vec[i]
-    esm_experiment <- esm_experiment_vec[j]
+    esm_name <- esm_name_vec[name_ind]
+    esm_experiment <- esm_experiment_vec[exp_ind]
     
+    print(name_ind)
+    print(exp_ind)
+    print(esm_name)
+    print(esm_experiment)
     
     rmarkdown::render(input = "Notebook8-emulate-validate.Rmd", output_file = paste0("Notebook8_", esm_name, "_", esm_experiment, "_tgav_validation_results.html"))
   }

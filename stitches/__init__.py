dba1f5a5b52a6c282472d183744eb9cc67dc9cd8
kdorheim <kalyn.dorheim@pnnl.gov>
2021-08-27 17:21:12 -0400

    functionalize tas archive

--- stitches/__init__.py
+++ stitches/__init__.py
@@ -1,3 +1,4 @@
 from .fx_match import match_neighborhood
 from .fx_recepie import permute_stitching_recipes, generate_gridded_recipe
 from .fx_stitch import stitching
+from .make_tas_archive import make_tas_archive

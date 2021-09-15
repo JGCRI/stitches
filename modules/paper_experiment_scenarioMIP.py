d46548ae67d3f1ece08956f711d7f37ed6b85d93
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2021-09-15 18:09:53 -0400

    script for scneariomip experiment that actually outputs netcdf files

--- modules/paper_experiment_scenarioMIP.py
+++ modules/paper_experiment_scenarioMIP.py
@@ -4,6 +4,9 @@
 # Using tol=0.08degC (min maxTol found in offline experiments), to be
 # safe since we haven't found maxTol for each ESM yet.
 
+# would be better to functionalize this script with ESM, tol and Ndraws as arguments
+# and then have the .sh just call the function and dispatch to diff nodes for each run I guess.
+
 # #############################################################################
 # General setup
 # #############################################################################
@@ -14,6 +17,15 @@ import pkg_resources
 
 pd.set_option('display.max_columns', None)
 
+OUTPUT_DIR = pkg_resources.resource_filename('stitches', 'data/created_data')
+
+
+# #############################################################################
+# Experiment  setup
+# #############################################################################
+# experiment parameters
+tolerance = 0.08
+Ndraws = 1
 
 # pangeo table of ESMs for reference
 pangeo_path = pkg_resources.resource_filename('stitches', 'data/pangeo_table.csv')
@@ -25,50 +37,101 @@ pangeo_126_esms.sort()
 pangeo_585_esms = pangeo_data[(pangeo_data['experiment'] == 'ssp585')].model.unique().copy()
 pangeo_585_esms.sort()
 
-print(pd.DataFrame({'model_126' : pangeo_126_esms, 'model_585' : pangeo_585_esms}))
 
-# esms = pangeo_126_esms
-esms = ['ACCESS-CM2', 'ACCESS-ESM1-5', 'AWI-CM-1-1-MR', 'BCC-CSM2-MR',
-       'CAMS-CSM1-0', 'CAS-ESM2-0', 'CESM2', 'CESM2-WACCM',
-       'CMCC-CM2-SR5', 'CMCC-ESM2', 'CanESM5', 'FGOALS-g3', 'FIO-ESM-2-0',
-       'GISS-E2-1-G', 'HadGEM3-GC31-LL', 'HadGEM3-GC31-MM', 'IITM-ESM',
-       'MCM-UA-1-0', 'MIROC-ES2L', 'MIROC6', 'MPI-ESM1-2-HR',
-       'MPI-ESM1-2-LR', 'MRI-ESM2-0', 'NESM3', 'NorESM2-LM', 'NorESM2-MM',
-       'TaiESM1', 'UKESM1-0-LL']
+esms = pangeo_126_esms
+#       ['ACCESS-CM2', 'ACCESS-ESM1-5', 'AWI-CM-1-1-MR', 'BCC-CSM2-MR',
+#        'CAMS-CSM1-0', 'CAS-ESM2-0', 'CESM2', 'CESM2-WACCM',
+#        'CMCC-CM2-SR5', 'CMCC-ESM2', 'CanESM5', 'FGOALS-g3', 'FIO-ESM-2-0',
+#        'GISS-E2-1-G', 'HadGEM3-GC31-LL', 'HadGEM3-GC31-MM', 'IITM-ESM',
+#        'MCM-UA-1-0', 'MIROC-ES2L', 'MIROC6', 'MPI-ESM1-2-HR',
+#        'MPI-ESM1-2-LR', 'MRI-ESM2-0', 'NESM3', 'NorESM2-LM', 'NorESM2-MM',
+#        'TaiESM1', 'UKESM1-0-LL']
 
 
-# Load the archive data we want to match on.
-archive_path = pkg_resources.resource_filename('stitches', 'data/matching_archive.csv')
-data = pd.read_csv(archive_path)
+# #############################################################################
+# The experiment
+# #############################################################################
 
-# Select the some data to use as the target data
-target_data = data[data["model"] == 'BCC-CSM2-MR']
-target_data = target_data[target_data["experiment"] == 'ssp245']
-target_data = target_data[target_data["ensemble"].isin(['r4i1p1f1', 'r1i1p1f1'])]
-target_data = target_data.reset_index(drop=True)
+# Load the full archive of all staggered windows, which we will be matching on
+full_archive_path = pkg_resources.resource_filename('stitches', 'data/matching_archive_staggered.csv')
+full_archive_data = pd.read_csv(full_archive_path)
 
-# Select the data to use as our archive.
-archive_data = data[data['model'] == 'BCC-CSM2-MR'].copy()
+# Load the original archive without staggered windows, which we will draw
+# the target trajectories from for matching
+full_target_path = pkg_resources.resource_filename('stitches', 'data/matching_archive.csv')
+full_target_data = pd.read_csv(full_target_path)
 
-# Use the match_neighborhood function to generate all of the matches between the target and
-# archive data points.
-match_df = stitches.match_neighborhood(target_data, archive_data, tol=0.1)
+esms = ['ACCESS-CM2']
+# for each of the esms in the experiment, subset to what we want
+# to work with and run the experiment.
+for esm in esms:
+    print(esm)
 
-# So the permute stitiching recipe works it works when N matches is set to 1 see blow for an
-# example where the function fails to return 2 matches per target.
-unformatted_recipe = stitches.permute_stitching_recipes(N_matches=4, matched_data=match_df, archive=archive_data)
+    # subset the archive and the targets to this ESM
+    archive_data = full_archive_data[(full_archive_data['model'] == esm) &
+                                     ((full_archive_data['experiment'] == 'ssp126') |
+                                      (full_archive_data['experiment'] == 'ssp585'))].copy()
 
-# The next step is to clean up the recipe so that it can be used to generate the gridded data products.
-# TODO this should be wrapped in a function that adds the other variables to the recpies
-# TODO and the everything should be wrapped in a command that does the make grided recpie.
-recipe = stitches.generate_gridded_recipe(unformatted_recipe)
-recipe.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
-                  'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
-                  'archive_end_yr', 'tas_file']
+    target_245 = full_target_data[(full_target_data['model'] == esm) &
+                                  (full_target_data['experiment'] == 'ssp245')].copy()
 
-# Get the gridded stitching & the global mean stitched products!
-outputs = stitches.gridded_stitching('.', recipe)
-outputs = stitches.gmat_stitching(recipe)
+    target_370 = full_target_data[(full_target_data['model'] == esm) &
+                                  (full_target_data['experiment'] == 'ssp370')].copy()
+
+    # Use the match_neighborhood function to generate all of the matches between the target and
+    # archive data points.
+    match_245_df = stitches.match_neighborhood(target_245, archive_data,
+                                               tol=tolerance)
+
+    match_370_df = stitches.match_neighborhood(target_370, archive_data,
+                                               tol=tolerance)
+
+    for draw in range(0, Ndraws):
+        # use the permute_stitching_recipes function to do the draws.
+        # Make N_matches very large just so the full collapse free ensemble is generated
+        unformatted_recipe_245 = stitches.permute_stitching_recipes(N_matches=10000,
+                                                                    matched_data=match_245_df,
+                                                                    archive=archive_data)
+        unformatted_recipe_245.to_csv('unform_rp.csv')
+
+        unformatted_recipe_370 = stitches.permute_stitching_recipes(N_matches=10000,
+                                                                    matched_data=match_370_df,
+                                                                    archive=archive_data)
+
+        # Clean up the recipe so that it can be used to generate the gridded data products.
+        recipe_245 = stitches.generate_gridded_recipe(unformatted_recipe_245)
+        recipe_245.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
+                              'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
+                              'archive_end_yr', 'tas_file']
+        recipe_245.to_csv('form_rp.csv')
+
+        recipe_370 = stitches.generate_gridded_recipe(unformatted_recipe_370)
+        recipe_370.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
+                              'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
+                              'archive_end_yr', 'tas_file']
+
+        # stitch the GSAT values and save as csv
+        gsat_245 = stitches.gmat_stitching(recipe_245)
+        for id in gsat_245.stitching_id.unique():
+            ds = gsat_245[gsat_245['stitching_id'] == id].copy()
+            fname = (OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
+                     'stitched_' + esm + '_GSAT_' + id + '.csv')
+            ds.to_csv(fname)
+
+        gsat_370 = stitches.gmat_stitching(recipe_370)
+        for id in gsat_370.stitching_id.unique():
+            ds = gsat_370[gsat_370['stitching_id'] == id].copy()
+            fname = (OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
+                     'stitched_' + esm + '_GSAT_' + id + '.csv')
+            ds.to_csv(fname)
+
+        # form and output the global gridded stitched products
+        outputs = stitches.gridded_stitching((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP'), recipe_245)
+        outputs = stitches.gridded_stitching((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP'), recipe_370)
+
+        # end for loop over draws
+    # end for loop over ESMs
+
 
 
 

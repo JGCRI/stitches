c07c28494cdaa3204fa93ac885945e78f1455e39
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2021-09-16 09:18:00 -0400

    scenarioMIP update output dir path for pic

--- modules/paper_experiment_scenarioMIP.py
+++ modules/paper_experiment_scenarioMIP.py
@@ -17,8 +17,8 @@ import pkg_resources
 
 # pd.set_option('display.max_columns', None)
 
-OUTPUT_DIR = pkg_resources.resource_filename('stitches', 'data/created_data')
-
+# OUTPUT_DIR = pkg_resources.resource_filename('stitches', 'data/created_data')
+OUTPUT_DIR = '/pic/projects/GCAM/stitches_pic/paper1_outputs'
 
 # #############################################################################
 # Experiment  setup

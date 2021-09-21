99fecce193927eefa44a568703b36b46523a0621
Snyder, Abigail C <abigail.snyder@pnnl.gov>
2021-09-21 09:27:13 -0400

    update scenarioMIP experiment with existence checks for each target

--- modules/paper_experiment_scenarioMIP.py
+++ modules/paper_experiment_scenarioMIP.py
@@ -38,7 +38,7 @@ pangeo_585_esms = pangeo_data[(pangeo_data['experiment'] == 'ssp585')].model.uni
 pangeo_585_esms.sort()
 
 
-esms = pangeo_126_esms[5:,]
+esms = pangeo_126_esms[12:,]
       # ['ACCESS-CM2', 'ACCESS-ESM1-5', 'AWI-CM-1-1-MR', 'BCC-CSM2-MR',
       #  'CAMS-CSM1-0', 'CAS-ESM2-0', 'CESM2', 'CESM2-WACCM',
       #  'CMCC-CM2-SR5', 'CMCC-ESM2', 'CanESM5', 'FGOALS-g3', 'FIO-ESM-2-0',
@@ -80,53 +80,79 @@ for esm in esms:
 
     # Use the match_neighborhood function to generate all of the matches between the target and
     # archive data points.
-    match_245_df = stitches.match_neighborhood(target_245, archive_data,
-                                               tol=tolerance)
+    if not target_245.empty:
+        match_245_df = stitches.match_neighborhood(target_245, archive_data,
+                                                   tol=tolerance)
+    else:
+        print('No target ssp245 data for ' + esm + '. Analysis will be skipped')
 
-    match_370_df = stitches.match_neighborhood(target_370, archive_data,
-                                               tol=tolerance)
+    if not target_370.empty:
+        match_370_df = stitches.match_neighborhood(target_370, archive_data,
+                                                   tol=tolerance)
+    else:
+        print('No target ssp370 data for ' + esm + '. Analysis will be skipped')
 
     for draw in range(0, Ndraws):
         # use the permute_stitching_recipes function to do the draws.
         # Make N_matches very large just so the full collapse free ensemble is generated
-        unformatted_recipe_245 = stitches.permute_stitching_recipes(N_matches=10000,
-                                                                    matched_data=match_245_df,
-                                                                    archive=archive_data, testing=True)
+        if not target_245.empty:
+            unformatted_recipe_245 = stitches.permute_stitching_recipes(N_matches=10000,
+                                                                        matched_data=match_245_df,
+                                                                        archive=archive_data, testing=True)
+        else:
+            print('No target ssp245 data for ' + esm + '. Matching skipped')
 
-        unformatted_recipe_370 = stitches.permute_stitching_recipes(N_matches=10000,
-                                                                    matched_data=match_370_df,
-                                                                    archive=archive_data,testing=True)
+        if not target_370.empty:
+            unformatted_recipe_370 = stitches.permute_stitching_recipes(N_matches=10000,
+                                                                        matched_data=match_370_df,
+                                                                        archive=archive_data, testing=True)
+        else:
+            print('No target ssp370 data for ' + esm + '. Matching skipped')
+
 
         # Clean up the recipe so that it can be used to generate the gridded data products.
-        recipe_245 = stitches.generate_gridded_recipe(unformatted_recipe_245)
-        recipe_245.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
-                              'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
-                              'archive_end_yr', 'tas_file']
-        recipe_245.to_csv((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
-                     'gridded_recipes_' + esm + '_target245' + '.csv'), index=False)
+        if not target_245.empty:
+            recipe_245 = stitches.generate_gridded_recipe(unformatted_recipe_245)
+            recipe_245.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
+                                  'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
+                                  'archive_end_yr', 'tas_file']
+            recipe_245.to_csv((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
+                               'gridded_recipes_' + esm + '_target245' + '.csv'), index=False)
+        else:
+            print('No target ssp245 data for ' + esm + '. Recipe formatting skipped')
 
-        recipe_370 = stitches.generate_gridded_recipe(unformatted_recipe_370)
-        recipe_370.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
-                              'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
-                              'archive_end_yr', 'tas_file']
-        recipe_370.to_csv((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
-                           'gridded_recipes_' + esm + '_target370' + '.csv'), index=False)
+        if not target_370.empty:
+            recipe_370 = stitches.generate_gridded_recipe(unformatted_recipe_370)
+            recipe_370.columns = ['target_start_yr', 'target_end_yr', 'archive_experiment', 'archive_variable',
+                                  'archive_model', 'archive_ensemble', 'stitching_id', 'archive_start_yr',
+                                  'archive_end_yr', 'tas_file']
+            recipe_370.to_csv((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
+                               'gridded_recipes_' + esm + '_target370' + '.csv'), index=False)
+        else:
+            print('No target ssp370 data for ' + esm + '. Recipe formatting skipped')
 
         # stitch the GSAT values and save as csv
         try:
-            gsat_245 = stitches.gmat_stitching(recipe_245)
-            for id in gsat_245.stitching_id.unique():
-                ds = gsat_245[gsat_245['stitching_id'] == id].copy()
-                fname = (OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
-                         'stitched_' + esm + '_GSAT_' + id + '.csv')
-                ds.to_csv(fname, index=False)
+            if not target_245.empty:
+                gsat_245 = stitches.gmat_stitching(recipe_245)
+                for id in gsat_245.stitching_id.unique():
+                    ds = gsat_245[gsat_245['stitching_id'] == id].copy()
+                    fname = (OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
+                             'stitched_' + esm + '_GSAT_' + id + '.csv')
+                    ds.to_csv(fname, index=False)
+            else:
+                print('No target ssp245 data for ' + esm + '. GSAT stitching skipped')
 
-            gsat_370 = stitches.gmat_stitching(recipe_370)
-            for id in gsat_370.stitching_id.unique():
-                ds = gsat_370[gsat_370['stitching_id'] == id].copy()
-                fname = (OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
-                         'stitched_' + esm + '_GSAT_' + id + '.csv')
-                ds.to_csv(fname, index=False)
+            if not target_370.empty:
+                gsat_370 = stitches.gmat_stitching(recipe_370)
+                for id in gsat_370.stitching_id.unique():
+                    ds = gsat_370[gsat_370['stitching_id'] == id].copy()
+                    fname = (OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP/' +
+                             'stitched_' + esm + '_GSAT_' + id + '.csv')
+                    ds.to_csv(fname, index=False)
+            else:
+                print('No target ssp370 data for ' + esm + '. GSAT stitching skipped')
+
         #end try
         except:
             print(("Some issue stitching GMAT for " + esm +". Skipping and moving on"))
@@ -134,15 +160,21 @@ for esm in esms:
 
         # form and output the global gridded stitched products
         try:
-            outputs = stitches.gridded_stitching((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP'), recipe_245)
-            outputs = stitches.gridded_stitching((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP'), recipe_370)
+            if not target_245.empty:
+                outputs = stitches.gridded_stitching((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP'), recipe_245)
+            else:
+                print('No target ssp245 data for ' + esm + '. Gridded stitching skipped')
+
+            if not target_370.empty:
+                outputs = stitches.gridded_stitching((OUTPUT_DIR + '/' + esm + '/experiment_scenarioMIP'), recipe_370)
+            else:
+                print('No target ssp370 data for ' + esm + '. Gridded stitching skipped')
+
         except:
             print(("Some issue stitching gridded for " + esm + ". Skipping and moving on"))
 
-
-
-        # end for loop over draws
-    # end for loop over ESMs
+    # end for loop over draws
+# end for loop over ESMs
 
 
 

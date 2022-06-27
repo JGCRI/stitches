0cc14ffa650299330dc7c9f12a4916db9ed6b397
Kalyn R. Dorheim <kalyn.dorheim@pnnl.gov>
2022-06-27 09:44:32 -0400

    simplify testing (#54)
    Simplify the testing for github actions, only go a pangeo function test to make sure the CI environment is set up properly.

---
+++ stitches/tests/test_pangeo.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import unittest
+import xarray as xr
+
+from stitches.fx_pangeo import fetch_pangeo_table, fetch_nc
+
+class TestPangeo(unittest.TestCase):
+    """Tests for the our pangeo helper functions."""
+
+    def test_get_pangeo_data(self):
+
+        # Get the table of all pangeo contents as a data frame
+        ptable = fetch_pangeo_table()
+        self.assertEqual(type(ptable), pd.core.frame.DataFrame, 'fetch_pangeo_table did not return a data frame')
+
+        # Select a single files from the pangeo cataog to import
+        import_this = ptable.loc[(ptable["table_id"] == "Amon") & (ptable["activity_id"] == "ScenarioMIP")].reset_index(drop = True)
+        path = import_this["zstore"][0]
+        out = fetch_nc(path)
+        self.assertEqual(type(out), xr.core.dataset.Dataset, 'problem with fetch_nc')
+
+
+if __name__ == '__main__':
+    unittest.main()

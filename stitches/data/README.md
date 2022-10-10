0a30c009bd5808cee25875202584c937b8174461
kdorheim <kalyn.dorheim@pnnl.gov>
2022-10-10 13:18:04 -0400

    add capabilites to download data from zenodo

---
+++ stitches/data/README.md
@@ -0,0 +1,6 @@
+Space holder for package data. Install the stitches package data by running:
+
+```bash
+import stitches 
+stitches.install_package_data()
+```
---
+++ stitches/install_pkgdata.py
@@ -0,0 +1,97 @@
+
+import os
+import tempfile
+import zipfile
+import shutil
+
+import requests
+
+import pkg_resources
+from io import BytesIO as BytesIO
+
+
+class InstallPackageData:
+    """Download and unpack example data minted on Zenodo that matches the current installed
+    stitches distribution.
+
+    :param data_dir:    Optional, Full path oto the directory you wish to store the data in. Default
+                        os to install it in the data directory of the package.
+
+    :type data_dir:     str
+    """
+
+    # URL for DOI minted example data hosted on Zenodo
+    DATA_VERSION_URLS = {'0.9.1':'https://zenodo.org/record/7181977/files/data.zip?download=1'}
+
+    def __init__(self, data_dir=None):
+
+        self.data_dir = data_dir
+
+    def fetch_zenodo(self):
+        """Download and unpack the Zenodo minted data for the
+        current stitches distribution."""
+
+        # full path to the stitches root directory where the example dir will be stored
+        if self.data_dir is None:
+            data_directory = pkg_resources.resource_filename('stitches', 'data')
+        else:
+            data_directory = self.data_dir
+
+        # get the current version of stitches that is installed
+        current_version = pkg_resources.get_distribution('stitches').version
+
+        try:
+            data_link = InstallPackageData.DATA_VERSION_URLS[current_version]
+
+        except KeyError:
+            msg = f"Link to data missing for current version:  {current_version}.  Please contact admin."
+
+            raise KeyError(msg)
+
+        # retrieve content from URL
+        print("Downloading example data for stitches version {}...".format(current_version))
+        r = requests.get(data_link)
+
+        with zipfile.ZipFile(BytesIO(r.content)) as zipped:
+
+            # extract each file in the zipped dir to the project
+            for f in zipped.namelist():
+
+                extension = os.path.splitext(f)[-1]
+
+                # Extract only the csv files
+                if all([len(extension) > 0, extension == ".csv"]):
+
+
+                    basename = os.path.basename(f)
+
+                    # Check to see if tas-data is in the file path
+                    if "tas-data" in f:
+                        basename = "tas-data/" + basename
+
+                    out_file = os.path.join(data_directory, basename)
+
+                    # extract to a temporary directory to be able to only keep the file out of the dir structure
+                    with tempfile.TemporaryDirectory() as tdir:
+
+                        # extract file to temporary directory
+                        zipped.extract(f, tdir)
+
+                        # construct temporary file full path with name
+                        tfile = os.path.join(tdir, f)
+
+                        print(f"Unzipped: {out_file}")
+                        # transfer only the file sans the parent directory to the data package
+                        shutil.copy(tfile, out_file)
+
+def install_package_data(data_dir=None):
+    """Download and unpack Zenodo minted that matches the current installed
+    stitches distribution.
+    :param data_dir:                    Optional.  Full path to the directory you wish to store the data in.  Default is
+                                        to install it in data directory of the package.
+    :type data_dir:                     str
+    """
+
+    zen = InstallPackageData(data_dir=data_dir)
+
+    zen.fetch_zenodo()

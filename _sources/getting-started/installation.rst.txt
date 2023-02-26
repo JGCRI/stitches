689dcb1ace73bb663dc3c7a3087f7bda5429d3fd
crvernon <chrisrvernon@gmail.com>
2023-02-26 08:42:26 -0500

    initial docs

---
+++ _sources/getting-started/installation.rst.txt
@@ -0,0 +1,17 @@
+Installation
+============
+
+Install ```stitches``` using pip:
+
+.. code-block:: console
+
+  $ # stitches requires python >= 3.8.
+  $ pip install stitches
+
+You may also install ```stitches``` from source.
+
+.. code-block:: console
+
+  $ git clone https://github.com/JGCRI/stitches.git
+  $ cd stitches
+  $ pip install .
--- stitches/__init__.py
+++
@@ -1,6 +0,0 @@
-from .fx_match import match_neighborhood
-from .fx_recepie import permute_stitching_recipes, generate_gridded_recipe, make_recipe
-from .fx_stitch import gridded_stitching, gmat_stitching
-from .make_tas_archive import make_tas_archive
-from .make_matching_archive import make_matching_archive
-from .make_pangeo_table import make_pangeo_table, make_pangeo_comparison

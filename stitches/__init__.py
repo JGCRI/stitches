840e45b58acb9af770a41acbeed42446cd03fb23
crvernon <chrisrvernon@gmail.com>
2023-04-04 12:20:06 -0400

    add package data import

--- stitches/__init__.py
+++ stitches/__init__.py
@@ -6,6 +6,7 @@ from .make_tas_archive import make_tas_archive
 from .make_matching_archive import make_matching_archive
 from .make_pangeo_table import make_pangeo_table, make_pangeo_comparison
 from .install_pkgdata import install_package_data
+from .package_data import *
 
 
 __version__ = "0.9.1"
--- stitches/install_pkgdata.py
+++ stitches/install_pkgdata.py
@@ -16,10 +16,13 @@ class InstallPackageData:
                         os to install it in the data directory of the package.
 
     :type data_dir:     str
+    
     """
 
     # URL for DOI minted example data hosted on Zenodo
-    DATA_VERSION_URLS = {'0.9.1':'https://zenodo.org/record/7181977/files/data.zip?download=1'}
+    DATA_VERSION_URLS = {'0.9.1': 'https://zenodo.org/record/7181977/files/data.zip?download=1',
+                         '0.10.0': 'https://zenodo.org/record/7181977/files/data.zip?download=1',
+                         '1.0.0': 'https://zenodo.org/record/7181977/files/data.zip?download=1'}
 
     DEFAULT_VERSION = 'https://zenodo.org/record/7181977/files/data.zip?download=1'
 
